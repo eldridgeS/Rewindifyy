3db5f4b55423bae7c5f2409fa3d4a1a8a420e672
Meeton <meetonk@gmail.com>
2024-11-01 13:28:21 -0400

    login template page

--- myappp/views.py
+++ myappp/views.py
@@ -6,3 +6,5 @@ def index(request):
     return render(request, 'myappp/home.html')
 def base(request):
     return render(request, 'myappp/base.html')
+def login(request):
+    return render(request, 'myappp/login.html')
